548a83f3b630f4ed42f93c6796908d0bdf22dbd7
maky189 <maky188pgt555@gmail.com>
2025-03-09 15:05:07 -0100

    Implemented Phantom Functions

--- Pacman.c
+++ Pacman.c
@@ -39,7 +39,7 @@ struct TPhantom {
     int direction, step, partial;
     int decided_turn, begin_turn;
     int actual_index;
-    int *patch;
+    int *path;
 };
 
 struct TVertex {
@@ -343,7 +343,7 @@ Pacman* pacman_create(int x, int y)
 }
 
 // Functions that free all data related to Pacman
-int pacman_destroy(Pacman *pac)
+void pacman_destroy(Pacman *pac)
 {
     free(pac);
 }
@@ -521,3 +521,452 @@ static void pacman_deathanimation(float column, float line, Pacman *pac)
 
 
 // NEXT IS PHANTOM
+static void phantom_move(Phantom *ph, int direction, Scenario *scen);
+static int phantom_DirectionGraph(Phantom *ph, Scenario *scen);
+static int phantom_DistanceGraph(Scenario *scen, int noA, int noB);
+static int phantom_DrawsDirection(Phantom *ph, Scenario *scen);
+static int phantom_SeePacman(Phantom *ph, Pacman *pac, Scenario *scen, int direction);
+static void phantom_SearchBestPath(Phantom *ph, Scenario *scen);
+static int phantom_MovingPhantomAlive(Phantom *ph, Pacman *pac, Scenario *scen);
+static int phantom_MovingPhantomDead(Phantom *ph, Scenario *scen);
+
+// Function that initializes the data from the phantom
+Phantom* phantom_create(int x, int y)
+{
+    Phantom* ph = malloc(sizeof(Phantom));
+    if(ph != NULL)
+    {
+        ph->step = 3;
+        ph->decided_turn = 0;
+        ph->begin_turn = 0;
+        ph->actual_index = 0;
+        ph->status = 0;
+        ph->direction = 0;
+        ph->partial = 0;
+        ph->xi = x;
+        ph->yi = y;
+        ph->x = x;
+        ph->y = y;
+        ph->path = NULL;
+    }
+    return ph;
+}
+
+// Function that liberates the data from the phantom
+void phantom_destroy(Phantom *ph)
+{
+    if(ph->path != NULL)
+    {
+        free(ph->path);
+    }
+    free(ph);
+}
+
+// Function that draws a phantom
+void phantom_draw(Phantom *ph)
+{
+    float line, column;
+    float step = (ph->partial/(float)block);
+
+    //Check position
+    if(ph->direction == 0 || ph->direction == 2)
+    {
+        line = ph->y;
+        column = ph->x + step;
+    }
+    else
+    {
+        line = ph->y + step;
+        column = ph->x;
+    }
+
+    // Chooses the sprite based on the direction and status(alive, dead, vulnerable)
+    int idx = 3*ph->direction + ph->status;
+    drawSprite(MAT2X(column), MAT2Y(line), phantomTex2d[idx]);
+}
+
+// Updates the position of a phantom
+void phantom_moving(Phantom *ph, Scenario *scen, Pacman *pac)
+{
+    int d;
+    if(ph->status == 1)
+    {
+        //If a phantom is dead, he commes back through the path of the graph
+        d = phantom_MovingPhantomDead(ph, scen);
+    }
+    else
+    {
+        //Run or persue Pacman? ? or 0
+        if(pacman_is_invencible(pac))
+        {
+            ph->status = 2;
+        }
+        else
+        {
+            ph->status = 0;
+        }
+
+        // Calls the function that calculates the new direction to where the phantom
+        // should be movinng with base on the map and the position of pacman
+        d = phantom_MovingPhantomAlive(ph, pac, scen);
+        
+        //What to do if touches pacman?
+        if(pac->x == ph->x && pac->y == ph->y)
+        {
+            if(pacman_is_invencible(pac))
+            {
+                ph->status = 1; //he is dead kkkk
+                pacman_points_ghosts(pac);
+                ph->begin_turn = 0;
+            }
+            else
+            {
+                if(pacman_alive(pac))
+                {
+                    pacman_dies(pac);
+                }
+            }
+        }
+    }
+    // Moving and drawing the phantom on screen
+    phantom_move(ph, d, scen);
+}
+
+// Updates the position of a phantom
+static void phantom_move(Phantom *ph, int direction, Scenario *scen)
+{
+    int xt = ph->x;
+    int yt = ph->y;
+
+    //Increments the position inside of a square of the matrix or changes the square
+    if(scen->map[ph->y + directions[direction].y][ph->x + directions[direction].x] <= 2)
+    {
+        if(direction == ph->direction)
+        {
+            if(ph->direction < 2)
+            {
+                ph->partial += ph->step;
+                if(ph->partial >= block)
+                {
+                    ph->x += directions[direction].x;
+                    ph->y += directions[direction].y;
+                    ph->partial = 0;
+                }
+            }
+            else
+            {
+                ph->partial -= ph->step;
+                if(ph->partial <= -block)
+                {
+                    ph->x += directions[direction].x;
+                    ph->y += directions[direction].y;
+                    ph->partial = 0;
+                }
+            }
+        }
+        else
+        {
+            // Change direction...
+            if(abs(direction - ph->direction) != 2)
+            {
+                ph->partial = 0;
+            }
+            ph->direction = direction;
+        }
+    }
+
+    if(xt != ph->x || yt != ph->y)
+    {
+        ph->decided_turn = 0;
+    }
+}
+
+// Function that helps to chose the path when the phantom dies
+static int phantom_Direction_Graph(Phantom *ph, Scenario *scen)
+{
+    if(scen->graph[ph->actual_index].x == scen->graph[ph->path[ph->actual_index]].x)
+    {
+        if(scen->graph[ph->actual_index].y > scen->graph[ph->path[ph->actual_index]].y)
+        {
+            return 3;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        if(scen->graph[ph->actual_index].x > scen->graph[ph->path[ph->actual_index]].x)
+        {
+            return 2;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
+
+// Function that helps in  choosing the path when the phantom dies
+static int phantom_DistanceGraph(Scenario *scen, int noA, int noB)
+{
+    return abs(scen->graph[noA].x - scen->graph[noB].x) + abs(scen->graph[noA].y - scen->graph[noB].y);
+}
+
+// When the phantom finds a turn, he draws a new direction
+// He tends to move forward, sometimes change directions and almost never go back the same path
+static int phantom_DrawsDirection(Phantom *ph, Scenario *scen)
+{
+    int i, j, k, max;
+    int chances[4], dir[4];
+
+    for(i = 0; i < 4; i++)
+    {
+        chances[i] = rand() % 10 + 1;
+    }
+
+    chances[ph->direction] = 7;
+    chances[(ph->direction + 2) % 4] = 3;
+
+    // Orders the chances of each direction
+    for(j = 0; j < 4; j++)
+    {
+        max = 0;
+        for(i = 0; i < 4; i++)
+        {
+            if(chances[i] > max)
+            {
+                max = chances[i];
+                k = i;
+            }
+        }
+        dir[j] = k;
+        chances[k] = 0;
+    }
+
+    // Choses the first valid direction
+    i = 0;
+    while(scen->map[ph->y + directions[dir[i]].y][ph->x + directions[dir[i]].x] > 2)
+    {
+        i++;
+    }
+    return dir[i];
+}
+
+
+// Function that groups all the cases of choice of direction of the phantom
+static int phantom_MovingPhantomAlive(Phantom *ph, Pacman *pac, Scenario *scen)
+{
+    int d, i;
+    if(scenario_IsTurn(ph->x, ph->y, scen))
+    {
+        if(!ph->decided_turn)
+        {
+            // On turn if Pacman, then persue
+            d = -1;
+            for(i = 0; i < 4; i++)
+            {
+                if(phantom_SeePacman(ph, pac, scen, i))
+                {
+                    d = i;
+                }
+            }
+            // Turn: Did not saw pacman, draw a direction
+            if(d == -1)
+            {
+                d = phantom_DrawsDirection(ph, scen);
+            }
+            else
+            {
+                // Turn: saw pacman, but he is invencible
+                // The RUN THE FUCK OUT MAN!! kkkk, draws new direction
+                if(pacman_is_invencible(pac))
+                {
+                    i = d;
+                    while(i == d)
+                    {
+                        d = phantom_DrawsDirection(ph, scen);
+                    }
+                }
+            }
+            ph->decided_turn = 1;
+        }
+        else
+        {
+            d = ph->direction;
+        }
+    }
+    else
+    {
+        // There is no turn: keep the same path
+        ph->decided_turn = 0;
+        d = ph->direction;
+        if(pacman_is_invencible(pac))
+        {
+            // If saw pacman ahead and he is invencible, go oposite way
+            if(phantom_SeePacman(ph, pac, scen, d))
+            {
+                d = (d + 2) % 4;
+            }
+        }
+
+        // Invert direction
+        if(scen->map[ph->y + directions[d].y][ph->x + directions[d].x] > 2)
+        {
+            d = (d + 2) % 4;
+        }
+    }
+    return d;
+}
+
+// Function that treats the cases where the phantom saw pacman
+static int phantom_SeePacman(Phantom *ph, Pacman *pac, Scenario *scen, int direction)
+{
+    int continum = 0;
+    if(direction == 0 || direction == 2)
+    {
+        if(pac->y == ph->y)
+        {
+            continum = 1;
+        }
+    }
+    else
+    {
+        if(pac->x == ph->x)
+        {
+            continum = 1;
+        }
+    }
+
+    if(continum)
+    {
+        int xt = ph->x;
+        int yt = ph->y;
+        while(scen->map[yt + directions[direction].y][xt + directions[direction].x] <= 2)
+        {
+            yt = yt + directions[direction].y;
+            xt = xt + directions[direction].x;
+
+            if(xt == pac->x && yt == pac->y)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+
+// Function that helps in search the best path when the phantom dies
+static void phantom_SearchBestPath(Phantom *ph, Scenario *scen)
+{
+    int i, k, index_on;
+    int continum, d;
+    int *dist;
+
+    dist = malloc(scen->NV*sizeof(int));
+    if(ph->path == NULL)
+    {
+        ph->path = malloc(scen->NV*sizeof(int));
+    }
+
+    //Begin calculating best path...
+    for(i = 0; i<scen->NV; i++)
+    {
+        dist[i] = 10000;
+        ph->path[i] = -1;
+        if(scen->graph[i].x == ph->xi && scen->graph[i].y == ph->yi)
+        {
+            index_on = i;
+        }
+        if(scen->graph[i].x == ph->x && scen->graph[i].y == ph->y)
+        {
+            ph->actual_index = i;
+        }
+    }
+
+    dist[index_on] = 0;
+
+
+    // calculates best path...
+    continum = 1;
+    while (continum)
+    {
+        continum = 0;
+        for(i = 0; i < scen->NV; i++)
+        {
+            for(k = 0; k < 4; k++)
+            {
+                if(scen->graph[i].neighbour[k] >= 0)
+                {
+                    d = phantom_DistanceGraph(scen, i, scen->graph[i].neighbour[k]);
+                    if(dist[scen->graph[i].neighbour[k]] > (dist[i] + d))
+                    {
+                        dist[scen->graph[i].neighbour[k]] = (dist[i] + d);
+                        ph->path[scen->graph[i].neighbour[k]] = i;
+                        continum = 1;
+                    }
+                }
+            }
+        }
+    }
+    free(dist);
+}
+
+// Function that helps choosing the path when the phantom dies
+static int phantom_MovingPhantomDead(Phantom *ph, Scenario *scen)
+{
+    int d;
+
+    if(!ph->begin_turn)
+    {
+        if(scenario_IsTurn(ph->x, ph->y, scen))
+        {
+            ph->begin_turn = 1;
+            phantom_SearchBestPath(ph, scen);
+            ph->decided_turn = 1;
+            d = phantom_Direction_Graph(ph, scen);
+        }
+        else
+        {
+            d = ph->direction;
+            if(scen->map[ph->y + directions[d].y][ph->x + directions[d].x] > 2) // Then wall
+            {
+                d = (d + 2) % 4;
+            }
+        }
+    }
+    else
+    {
+        // Make path back
+        if(ph->x != ph->xi || ph->y != ph->yi)
+        {
+            if(scenario_IsTurn(ph->x, ph->y, scen))
+            {
+                if(ph->decided_turn)
+                {
+                    d = ph->direction;
+                }
+                else
+                {
+                    //Checks the direction to take
+                    ph->actual_index = ph->path[ph->actual_index];
+                    d = phantom_DirectionGraph(ph, scen);
+                    ph->decided_turn = 1;
+                }
+            }
+            else
+            {
+                d = ph->direction;
+                ph->decided_turn = 0;
+            }
+        }
+        else
+        {
+            ph->status = 0;
+            d = ph->direction;
+        }
+    }
+    return d;
+}
